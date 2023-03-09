c54a5161d8cd96ddf73edd8cc85c4f83f060b259
McKay Harris <flybonzai@gmail.com>
2023-03-09 15:37:20 -0500

    PR feedback for fish changes

--- build.fish
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env fish
-
-set AWS_CLI_MFA_SHELL fish; and \
-string replace --all '"' '\\"' < ./src/aws_cli_mfa.py | sed -e "/#INSERT_PYTHON_CODE_HERE/r /dev/stdin" -e "s///" ./src/mfa.fish |
-    sudo tee /usr/local/bin/aws-cli-mfa >/dev/null &&
-    sudo chmod +x /usr/local/bin/aws-cli-mfa &&
-    cp /usr/local/bin/aws-cli-mfa ./bin/aws-cli-mfa-$AWS_CLI_MFA_SHELL
--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 AWS_CLI_MFA_SHELL=bash && \
-sed -e "/#INSERT_PYTHON_CODE_HERE/r ./src/aws_cli_mfa.py" -e "s///" ./src/$AWS_CLI_MFA_SHELL.sh |
-    sudo tee ./bin/aws-cli-mfa-$AWS_CLI_MFA_SHELL > /dev/null
+sed -e "/#INSERT_PYTHON_CODE_HERE/r ./src/aws_cli_mfa.py" -e "s///" ./src/$AWS_CLI_MFA_SHELL.sh > ./bin/aws-cli-mfa-$AWS_CLI_MFA_SHELL &&
+chmod +x ./bin/aws-cli-mfa-$AWS_CLI_MFA_SHELL
 
 AWS_CLI_MFA_SHELL=zsh && \
-sed -e "/#INSERT_PYTHON_CODE_HERE/r ./src/aws_cli_mfa.py" -e "s///" ./src/$AWS_CLI_MFA_SHELL.sh |
-    sudo tee ./bin/aws-cli-mfa-$AWS_CLI_MFA_SHELL > /dev/null
+sed -e "/#INSERT_PYTHON_CODE_HERE/r ./src/aws_cli_mfa.py" -e "s///" ./src/$AWS_CLI_MFA_SHELL.sh > ./bin/aws-cli-mfa-$AWS_CLI_MFA_SHELL &&
+chmod +x ./bin/aws-cli-mfa-$AWS_CLI_MFA_SHELL
 
 AWS_CLI_MFA_SHELL=ksh && \
-sed -e "/#INSERT_PYTHON_CODE_HERE/r ./src/aws_cli_mfa.py" -e "s///" ./src/$AWS_CLI_MFA_SHELL.sh |
-    sudo tee ./bin/aws-cli-mfa-$AWS_CLI_MFA_SHELL > /dev/null
+sed -e "/#INSERT_PYTHON_CODE_HERE/r ./src/aws_cli_mfa.py" -e "s///" ./src/$AWS_CLI_MFA_SHELL.sh > ./bin/aws-cli-mfa-$AWS_CLI_MFA_SHELL &&
+chmod +x ./bin/aws-cli-mfa-$AWS_CLI_MFA_SHELL
 
 AWS_CLI_MFA_SHELL=fish && \
-sed 's/"/\\\"/g' ./src/aws_cli_mfa.py | sed -e "/#INSERT_PYTHON_CODE_HERE/r /dev/stdin" -e "s///" ./src/mfa.fish |
-    sudo tee ./bin/aws-cli-mfa-$AWS_CLI_MFA_SHELL > /dev/null
+sed 's/"/\\\"/g' ./src/aws_cli_mfa.py | sed -e "/#INSERT_PYTHON_CODE_HERE/r /dev/stdin" -e "s///" ./src/mfa.fish > ./bin/aws-cli-mfa-$AWS_CLI_MFA_SHELL &&
+chmod +x ./bin/aws-cli-mfa-$AWS_CLI_MFA_SHELL
