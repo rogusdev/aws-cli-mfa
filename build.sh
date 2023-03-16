7716f049eb0b204725b31823599f21a62223f1a1
McKay Harris <flybonzai@gmail.com>
2023-03-16 17:21:34 -0400

    Add fish support and tests

--- build.sh
+++ build.sh
@@ -1,21 +1,17 @@
 #!/usr/bin/env bash
 
 AWS_CLI_MFA_SHELL=bash && \
-sed -e "/#INSERT_PYTHON_CODE_HERE/r ./src/aws_cli_mfa.py" -e "s///" ./src/$AWS_CLI_MFA_SHELL.sh |
-    sudo tee /usr/local/bin/aws-cli-mfa >/dev/null &&
-    sudo chmod +x /usr/local/bin/aws-cli-mfa &&
-    cp /usr/local/bin/aws-cli-mfa ./bin/aws-cli-mfa-$AWS_CLI_MFA_SHELL
+sed -e "/#INSERT_PYTHON_CODE_HERE/r ./src/aws_cli_mfa.py" -e "s///" ./src/$AWS_CLI_MFA_SHELL.sh > ./bin/aws-cli-mfa-$AWS_CLI_MFA_SHELL &&
+chmod +x ./bin/aws-cli-mfa-$AWS_CLI_MFA_SHELL
 
 AWS_CLI_MFA_SHELL=zsh && \
-sed -e "/#INSERT_PYTHON_CODE_HERE/r ./src/aws_cli_mfa.py" -e "s///" ./src/$AWS_CLI_MFA_SHELL.sh |
-    sudo tee /usr/local/bin/aws-cli-mfa >/dev/null &&
-    sudo chmod +x /usr/local/bin/aws-cli-mfa &&
-    cp /usr/local/bin/aws-cli-mfa ./bin/aws-cli-mfa-$AWS_CLI_MFA_SHELL
+sed -e "/#INSERT_PYTHON_CODE_HERE/r ./src/aws_cli_mfa.py" -e "s///" ./src/$AWS_CLI_MFA_SHELL.sh > ./bin/aws-cli-mfa-$AWS_CLI_MFA_SHELL &&
+chmod +x ./bin/aws-cli-mfa-$AWS_CLI_MFA_SHELL
 
 AWS_CLI_MFA_SHELL=ksh && \
-sed -e "/#INSERT_PYTHON_CODE_HERE/r ./src/aws_cli_mfa.py" -e "s///" ./src/$AWS_CLI_MFA_SHELL.sh |
-    sudo tee /usr/local/bin/aws-cli-mfa >/dev/null &&
-    sudo chmod +x /usr/local/bin/aws-cli-mfa &&
-    cp /usr/local/bin/aws-cli-mfa ./bin/aws-cli-mfa-$AWS_CLI_MFA_SHELL
+sed -e "/#INSERT_PYTHON_CODE_HERE/r ./src/aws_cli_mfa.py" -e "s///" ./src/$AWS_CLI_MFA_SHELL.sh > ./bin/aws-cli-mfa-$AWS_CLI_MFA_SHELL &&
+chmod +x ./bin/aws-cli-mfa-$AWS_CLI_MFA_SHELL
 
-sudo rm -f /usr/local/bin/aws-cli-mfa
+AWS_CLI_MFA_SHELL=fish && \
+sed 's/"/\\\"/g' ./src/aws_cli_mfa.py | sed -e "/#INSERT_PYTHON_CODE_HERE/r /dev/stdin" -e "s///" ./src/mfa.fish > ./bin/aws-cli-mfa-$AWS_CLI_MFA_SHELL &&
+chmod +x ./bin/aws-cli-mfa-$AWS_CLI_MFA_SHELL
